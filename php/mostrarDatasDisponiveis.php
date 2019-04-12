c0f4a571371e149656039bf18b04e1ed15c8d77f
Edilson Santana <eded1263@gmail.com>
2019-04-12 12:33:54 -0300

    Menu de opcoes na cadastroSessaoAdmin implementado, corrigir apenas o css

--- php/mostrarDatasDisponiveis.php
+++ php/mostrarDatasDisponiveis.php
@@ -2,7 +2,6 @@
     include "conexao.php";
 
     $query = "select id_data,DATE_FORMAT(data, '%d/%m/%Y') as data from datas ORDER BY data ASC";
-
     
     $resultado = $conexao->query($query);
 
@@ -11,18 +10,28 @@
             $query2 = "select id_horario_data,agendada,TIME_FORMAT(horario, '%H:%i') as horarioF from datahora where id_data = ".$registro["id_data"]." ORDER BY horario ASC";
             $resultado2 = $conexao->query($query2);
             $resultado3 = $conexao->query($query2);
-            echo 
-            "<tr>
+            echo "
+                <script>
+                    function mouseOver(i) {
+                      document.getElementById('opcoes'+i).style.display = 'block';
+                    }
+
+                    function mouseOut(i) {
+                      document.getElementById('opcoes'+i).style.display = 'none';
+                    }
+                    </script>
+                <tr>
                 <td style= 'text-align: center; 
                             color: black; 
                             border: 1px solid black; padding: 5px;'>".$registro["data"]."</td>
                 
                 <td style= 'text-align: center; 
                             color: black; 
-                            border: 1px solid black; padding: 5px;' class='tdHorario'>
-                  </div>";
+                            border: 1px solid black; padding: 5px;' >
+                  ";
             while($registroHoras = $resultado2->fetch_assoc()){
-                echo "<div class='opcoes'><a href='editarHorarios.php?id_horario=".$registroHoras["id_horario_data"]."'>Editar</a> || <a href='apagarHorarios.php?id_horario=".$registroHoras["id_horario_data"]."'>Apagar</a></div>".$registroHoras["horarioF"]."<br />";;
+                echo "<div style='display:none;'id='opcoes".$registroHoras["id_horario_data"]."'><a href='editarHorarios.php?id_horario=".$registroHoras["id_horario_data"]."'>Editar</a> || <a href='apagarHorarios.php?id_horario=".$registroHoras["id_horario_data"]."'>Apagar</a></div><span  onMouseOver='mouseOver(".$registroHoras["id_horario_data"].")'
+   onMouseOut='mouseOut(".$registroHoras["id_horario_data"].")' style='display:inline'>".$registroHoras["horarioF"]."</span><br />";
             }
             echo "
                     </td>

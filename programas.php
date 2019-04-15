8221b6f9c5b6096ba2a08816e577e8de32ac0d12
Letícia <lemayara16@gmail.com>
2019-04-15 12:40:16 -0300

    mudancas pagina programas

--- programas.php
+++ programas.php
@@ -5,79 +5,63 @@
 	<div class="services py-5" id="services">
 		<div class="container py-xl-5 py-lg-3">
 			<div class="ins-sec1">
-				<div class="row">
-					<div class="row mt-md-5 pt-md-5">
-						<div class="col-lg-4">
-							<div class="abt-block mb-lg-0 mb-md-5">
-								<div class="serv_abs serv_bottom" style="background-color: #00bcc5 !important"></div>
-								<h3>Pacote Clear</h3>
-								<p>Texto</p>
-							</div>
-						</div>
-						<div class="col-lg-4 my-lg-0 my-md-5">
-							<div class="abt-block">
-								<div class="serv_abs serv_bottom" style="background-color: #7b1887 !important"></div>
-								<h3>Pacote Confort</h3>
-								<p>Texto</p>
-							</div>
-						</div>
-						<div class="col-lg-4 mt-lg-0 mt-md-5">
-							<div class="abt-block">
-								<div class="serv_abs serv_bottom" style="background-color: #666666 !important"></div>
-								<h3>Pacote Axi</h3>
-								<p>Tratamento de <strong>10 sessões em todas as áreas do corpo </strong>  que o cliente desejar por um <strong>valor único e mensal</strong>.</p>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-8 mx-auto mt-lg-5">
-						<div class="row">
-							<div class="col-lg-6 my-lg-0 my-md-5">
-								<div class="abt-block mt-sm-0">
-									<div class="serv_abs  serv_bottom">
-										<span class="fa fa-user-md"></span>
-									</div>
-									<h3>SCRUBS MEDICINE</h3>
-									<p>Donec rutrum congue leo eget malesuada. Cras ultricies ligula sed magna
-										dictum
-										porta.
-									</p>
-								</div>
-							</div>
-							<div class="col-lg-6 mt-lg-0 mt-md-5">
-								<div class="abt-block">
-									<div class="serv_abs  serv_bottom">
-										<span class="fa fa-thumbs-o-up"></span>
-									</div>
-									<h3>WELL TRAINED EXPERTS</h3>
-									<p>Rutrum congue donec leo eget malesuada. Cras ultricies ligula sed magna
-										dictum
-										porta.
-									</p>
-								</div>
-							</div>
-						</div>
-					</div>
+				<div class="clubeaxi">
+					<h3 class="agileits-title titleProgramas" id="titleClubeAxi">Programa CLUBE AXI</h2> 
 				</div>
+				<br>
+				<br>
+					<div class="row">
+						<div class="row mt-md-5 pt-md-5">
+							<div class="col-lg-4">
+								<div class="abt-block mb-lg-0 mb-md-5">
+									<div class="serv_abs serv_bottom" style="background-color: #00bcc5 !important"></div>
+									<h3>Pacote Clear</h3>
+									<p>Possui a <strong>liberdade</strong> de escolha na quantidade de sessões que o cliente deseja, ou seja, o cliente escolhe se deseja fazer 03, 05 ou 10 sessões, <strong>sem mudar o valor do tratamento</strong>.</p>
+								</div>
+							</div>
+							<div class="col-lg-4 my-lg-0 my-md-5">
+								<div class="abt-block">
+									<div class="serv_abs serv_bottom" style="background-color: #7b1887 !important"></div>
+									<h3>Pacote Confort</h3>
+									<p>Texto</p>
+								</div>
+							</div>
+							<div class="col-lg-4 mt-lg-0 mt-md-5">
+								<div class="abt-block">
+									<div class="serv_abs serv_bottom" style="background-color: #b3b3b3!important"></div>
+									<h3>Pacote Axi</h3>
+									<p>Tratamento de <strong>10 sessões em todas as áreas do corpo </strong>  que o cliente desejar por um <strong>valor único e mensal</strong>.</p>
+								</div>
+							</div>
+						</div>
+						<hr>
+						<div class="axiblack">
+							<h3 class="agileits-title titleProgramas" id="titleAxiBlack">Tratamento Tradicional</h2> 
+						</div>
+						<br>
+						<br>
+						<div class="col-lg-8 mx-auto mt-lg-5">
+							<div class="row">
+								<div class="col-lg-6 my-lg-0 my-md-5">
+									<div class="abt-block mt-sm-0">
+										<div class="serv_abs  serv_bottom" style="background-color: #666666 !important"></div>
+										<h3>Sessão Avulsa</h3>
+										<p>Texto</p>
+									</div>
+								</div>
+								<div class="col-lg-6 mt-lg-0 mt-md-5">
+									<div class="abt-block">
+										<div class="serv_abs  serv_bottom" style="background-color: #666666 !important"></div>
+										<h3>Pacote 10 Sessões</h3>
+										<p>Texto</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 			</div>
 		</div>
 	</div>
-	<!-- about -->
-	<div class="about ">
-		<div class="container"> 
-			<!-- about-section2 -->
-			<!-- //about-section2 -->
-			<div class="clubeaxi">
-				<h3 class="agileits-title titleProgramas" id="titleClubeAxi">Programa CLUBE AXI</h2> 
-				<p>Possui a <strong>liberdade</strong> de escolha na quantidade de sessões que o cliente deseja, ou seja, o cliente escolhe se deseja fazer 03, 05 ou 10 sessões, <strong>sem mudar o valor do tratamento</strong>.</p>
-			</div>
-			<hr style="border-color: #7b1887">
-			<div class="axiblack">
-				<h3 class="agileits-title titleProgramas" id="titleAxiBlack">Tratamento Tradicional</h2> 
-				<p></p>
-			</div>
-		</div>
-	</div>
-	<!-- //about -->
 <?php  
 	require "footer.php";
 ?>

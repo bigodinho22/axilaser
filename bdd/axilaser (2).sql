e2d1d3298f1b4efa22f1acd5827745d538a3450d
Letícia <lemayara16@gmail.com>
2019-04-25 12:55:49 -0300

    banco vazio

--- bdd/axilaser (2).sql
+++ bdd/axilaser (2).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: 23-Abr-2019 às 20:01
--- Versão do servidor: 5.7.23
--- versão do PHP: 7.2.10
+-- Generation Time: 25-Abr-2019 às 15:55
+-- Versão do servidor: 5.7.21
+-- PHP Version: 5.6.35
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -36,14 +36,6 @@ CREATE TABLE IF NOT EXISTS `admin` (
   PRIMARY KEY (`login`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `admin`
---
-
-INSERT INTO `admin` (`login`, `senha`, `tipo`) VALUES
-('admin', '21232f297a57a5a743894a0e4a801fc3', 1),
-('admin1', '21232f297a57a5a743894a0e4a801fc3', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -63,16 +55,6 @@ CREATE TABLE IF NOT EXISTS `agenda` (
   KEY `id_cliente` (`id_cliente`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `agenda`
---
-
-INSERT INTO `agenda` (`id_agenda`, `id_cliente`, `titulo`, `id_horario_data`, `regiao`, `anotacoes`, `compareceu`) VALUES
-(6, 36, NULL, 12, 'Axila', NULL, 1),
-(7, 37, NULL, 13, 'Axila', NULL, 0),
-(8, 38, NULL, 11, 'Axila', NULL, 0),
-(9, 39, NULL, 27, 'Axila', NULL, 0);
-
 -- --------------------------------------------------------
 
 --
@@ -88,19 +70,7 @@ CREATE TABLE IF NOT EXISTS `cliente` (
   `telefone` varchar(120) NOT NULL,
   `senha` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id_cliente`)
-) ENGINE=InnoDB AUTO_INCREMENT=40 DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `cliente`
---
-
-INSERT INTO `cliente` (`id_cliente`, `nome`, `idade`, `email`, `telefone`, `senha`) VALUES
-(33, 'Edilson', 0, 'eded1263@gmail.com', '11972913914', '6f0450954442d9cfe4ef129d45798975'),
-(34, 'bigode', 0, 'bigode@gmail.com', '11972913914', '4edaa105d5f53590338791951e38c3ad'),
-(35, 'bruna', 0, 'bruna@gmail.com', '11972913914', '4edaa105d5f53590338791951e38c3ad'),
-(36, 'renato', 0, 'renato@gmail.com', '11972913914', '4edaa105d5f53590338791951e38c3ad'),
-(38, 'Edilson', 0, 'edilson@gmail.com', '11998075607', '202cb962ac59075b964b07152d234b70'),
-(39, 'Letícia ', 17, 'lemayara16@gmail.com', '11998075607', '202cb962ac59075b964b07152d234b70');
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -124,21 +94,6 @@ CREATE TABLE IF NOT EXISTS `clientesformulario` (
   PRIMARY KEY (`id_cliente_formulario`)
 ) ENGINE=MyISAM AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `clientesformulario`
---
-
-INSERT INTO `clientesformulario` (`id_cliente_formulario`, `nome`, `idade`, `email`, `telefone`, `fototipo`, `corPelo`, `roacutan`, `cancerVitiligo`, `pelo`, `situacao`) VALUES
-(2, 'flavio', 0, 'fla@gmail.com', '999999', '', '', '', '', '', 2),
-(4, 'edilson', 0, 'edilson@gmail.com', '9999999999', '', '', '', '', '', 3),
-(7, 'andre', 0, 'andre@gmailc.om', '020392039', '', '', '', '', '', 1),
-(8, 'zeca', 0, 'zecaurubu@gmail.com', '0909090909', '', '', '', '', '', 3),
-(18, 'Letícia ', 0, 'lemayara16@gmail.com', '11998075607', '', '', '', '', '', 2),
-(19, 'ricardo', 0, 'rr@email.com', '11', '', '', '', '', '', 1),
-(28, 'Letícia ', 17, 'lele@gmail.com', '11998075607', 'VI', '2', '3', '3', '3', 3),
-(29, 'Letícia', 17, 'l@gmail.com', '11998075607', 'VI', '2', '3', '3', '3', 2),
-(32, 'Edilson', 27, 'testeEdilson@gmail.com', '11972913914', 'V', '2', '3', '3', '2', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -155,19 +110,6 @@ CREATE TABLE IF NOT EXISTS `datahora` (
   KEY `id_data` (`id_data`)
 ) ENGINE=MyISAM AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `datahora`
---
-
-INSERT INTO `datahora` (`id_horario_data`, `id_data`, `horario`, `agendada`) VALUES
-(11, 10, '09:15:00', 1),
-(12, 10, '09:35:00', 1),
-(13, 10, '10:15:00', 1),
-(14, 10, '15:00:00', 0),
-(27, 25, '11:00:00', 1),
-(28, 25, '12:00:00', 0),
-(29, 25, '00:00:13', 0);
-
 -- --------------------------------------------------------
 
 --
@@ -179,15 +121,7 @@ CREATE TABLE IF NOT EXISTS `datas` (
   `id_data` int(5) NOT NULL AUTO_INCREMENT,
   `data` date NOT NULL,
   PRIMARY KEY (`id_data`)
-) ENGINE=MyISAM AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
-
---
--- Extraindo dados da tabela `datas`
---
-
-INSERT INTO `datas` (`id_data`, `data`) VALUES
-(10, '2019-04-04'),
-(25, '2019-07-03');
+) ENGINE=MyISAM AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
